6a2769936899578d17f2c26406d13be7bbc52e0c
Isaac Luton <isaacdluton@gmail.com>
2022-04-01 14:51:41 -0700

    addition of gitIgnore, addition of 5 day Api call

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
 
+const cityEl = document.querySelector(".city")
 const tempData = document.querySelector(".tempData");
 const windData = document.querySelector(".windData");
 const humidityData = document.querySelector(".humidityData");
 const uvIndexData = document.querySelector(".uvIndexData");
+const cityInput = document.querySelector("#cityInput");
+const units = document.querySelector("#unit");
+const searchBtn = document.querySelector(".searchBtn");
 
-const oneCallApi = "https://api.openweathermap.org/data/2.5/onecall?lat=47.6080131&lon=-122.335167&exclude={part}&units=imperial&appid=a215cfdcaaef2536002948ba6c285538"
-fetch(oneCallApi)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
-       
-        tempData.textContent = data.current.temp;
-        windData.textContent = data.current.wind_speed;
-        humidityData.textContent = data.current.humidity;
-        uvIndexData.textContent = data.current.uvi;
 
-        console.log(tempData)
-      }
-    );
+searchBtn.addEventListener("click", function () {
 
-  console.log(unit.value)
+
+  const oneCallApi = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value + "&appid=a215cfdcaaef2536002948ba6c285538&units=" + units.value;
+
+  const fiveDayAPI = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + "&appid=a215cfdcaaef2536002948ba6c285538";
+
+
+  fetch(oneCallApi)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+        
+          cityEl.textContent = data.name;
+          tempData.textContent = data.main.temp;
+          windData.textContent = data.wind.speed;
+          humidityData.textContent = data.main.humidity;
+          uvIndexData.textContent = data.main.uvi;
+
+        }
+      );
+
+  
+  
+      fetch(fiveDayAPI)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (dataTwo) {
+        console.log(dataTwo)
+
+        }
+      );
+  
+  });
+
+
+console.log(units.value)
