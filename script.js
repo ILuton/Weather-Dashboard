6a15125ad8815e5472401fe72773d8b77ff34300
Isaac Luton <isaacdluton@gmail.com>
2022-04-20 17:03:41 -0700

    adding weather icons and improve local storage functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const cityEl = document.querySelector(".city")
 const tempData = document.querySelector(".tempData");
+const tempDataMax = document.querySelector(".tempDataMax");
 const windData = document.querySelector(".windData");
 const humidityData = document.querySelector(".humidityData");
 const uvIndexData = document.querySelector(".uvIndexData");
@@ -14,12 +15,30 @@ const dayThreeEL = document.querySelector(".dayThree");
 const dayFourEL = document.querySelector(".dayFour");
 const dayFiveEL = document.querySelector(".dayFive");
 const fiveDayEL = document.querySelector(".fiveDay").getElementsByTagName("div");
-
+const weatherIconEl = document.querySelector(".weatherIcon");
+const weatherDesEl = document.querySelector(".weatherDes")
+const dayOneIconEL = document.querySelector(".dayOneIcon");
+const dayTwoIconEL = document.querySelector(".dayTwoIcon");
+const dayThreeIconEL = document.querySelector(".dayThreeIcon");
+const dayFourIconEL = document.querySelector(".dayFourIcon");
+const dayFiveIconEL = document.querySelector(".dayFiveIcon");
+const dayOneDesEl = document.querySelector(".dayOneDes");
+const dayTwoDesEl = document.querySelector(".dayTwoDes");
+const dayThreeDesEl = document.querySelector(".dayThreeDes");
+const dayFourDesEl = document.querySelector(".dayFourDes");
+const dayFiveDesEl = document.querySelector(".dayFiveDes");
+const stateInputEl = document.querySelector("#stateInput")
+const cityAppend = document.querySelector(".cityAppend")
+const cityEvent = document.querySelectorAll(".cityAppend li")
+let cities = [];
 
 searchBtn.addEventListener("click", function () {
 
-
-  const fiveDayApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + "&appid=a215cfdcaaef2536002948ba6c285538";
+  if (stateInputEl.value === "null") {
+    var fiveDayApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + "&appid=a215cfdcaaef2536002948ba6c285538";
+  } else {
+   fiveDayApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + ",US-" + stateInputEl.value.toUpperCase() + "&appid=a215cfdcaaef2536002948ba6c285538";
+  }
 
 
   fetch(fiveDayApi)
@@ -38,13 +57,44 @@ searchBtn.addEventListener("click", function () {
         
 
         // save searched cities to local storage and add to site
-        localStorage.setItem("city",cityEl.textContent)
-        let getCity = localStorage.getItem("city")
-        let getCityEL = document.createElement("h2")
 
-        getCityEL.textContent = getCity;
+        let storedCity = JSON.parse(localStorage.getItem("storedCity"))
 
-        citySearch.appendChild(getCityEL);
+        if (storedCity !== null) {
+          cities = storedCity
+            
+          }
+        
+
+
+
+        cities.push(data.city.name);
+
+        localStorage.setItem("storedCity", JSON.stringify(cities))
+
+
+        console.log(storedCity);
+
+        while( cityAppend.hasChildNodes() ){
+          cityAppend.removeChild(cityAppend.lastChild);
+      }
+
+      if (storedCity === null) {
+        let listEl = document.createElement("li");
+        listEl.textContent = cities;
+        cityAppend.appendChild(listEl)
+      } else {
+
+      for (let i = 0; i < cities.length; i++) {
+        
+        let listEl = document.createElement("li");
+        listEl.textContent = storedCity[i];
+        cityAppend.appendChild(listEl)
+      }
+    }
+
+
+
 
 
         const oneCallApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=a215cfdcaaef2536002948ba6c285538&units=" + units.value;
@@ -58,65 +108,115 @@ searchBtn.addEventListener("click", function () {
       
             //add current weather data to city box
             tempData.textContent = dataTwo.current.temp;
+            tempDataMax.textContent = dataTwo.daily[0].temp.day;
             windData.textContent = dataTwo.current.wind_speed;
             humidityData.textContent = dataTwo.current.humidity;
             uvIndexData.textContent = dataTwo.current.uvi;
+            let iconUrl = dataTwo.current.weather[0].icon
+            weatherIconEl.src = `https://openweathermap.org/img/wn/${iconUrl}@2x.png`
+            weatherDesEl.textContent = dataTwo.current.weather[0].description.toUpperCase()
+
+            if (dataTwo.current.uvi < 2) {
+              uvIndexData.style.backgroundColor = "green"
+            } else if (dataTwo.current.uvi > 2 && dataTwo.current.uvi < 5) {
+              uvIndexData.style.backgroundColor = "yellow"
+            } else if (dataTwo.current.uvi > 5 && dataTwo.current.uvi < 7) {
+              uvIndexData.style.backgroundColor = "orange"
+            } else {
+              uvIndexData.style.backgroundColor = "red"
+            }
 
 
             //populate 5 ay forcast data
+
+
             function dayOne () {
 
-              dayOneEL.children[0].textContent = data.list[3].dt_txt
+              dayOneEL.children[2].textContent = data.list[3].dt_txt.substring(0, 10)
             
-              dayOneEL.children[1].textContent = "Temp: " + dataTwo.daily[0].temp.day  
+              dayOneEL.children[3].textContent = "Temp: " + dataTwo.daily[0].temp.day  
             
-              dayOneEL.children[2].textContent = "Wind: " + dataTwo.daily[0].wind_speed
+              dayOneEL.children[4].textContent = "Wind: " + dataTwo.daily[0].wind_speed
             
-              dayOneEL.children[3].textContent = "Humidity: " + dataTwo.daily[0].humidity
+              dayOneEL.children[5].textContent = "Humidity: " + dataTwo.daily[0].humidity
+
+              let dayOneIconCode = dataTwo.daily[1].weather[0].icon
+
+              dayOneIconEL.src = `https://openweathermap.org/img/wn/${dayOneIconCode}@2x.png`
+
+              dayOneDesEl.textContent = dataTwo.daily[1].weather[0].description.toUpperCase()
             }
 
             function dayTwo () {
 
-              dayTwoEL.children[0].textContent = data.list[11].dt_txt
+              dayTwoEL.children[2].textContent = data.list[11].dt_txt.substring(0, 10)
             
-              dayTwoEL.children[1].textContent = "Temp: " + dataTwo.daily[1].temp.day  
             
-              dayTwoEL.children[2].textContent = "Wind: " + dataTwo.daily[1].wind_speed
+              dayTwoEL.children[3].textContent = "Temp: " + dataTwo.daily[1].temp.day  
             
-              dayTwoEL.children[3].textContent = "Humidity: " + dataTwo.daily[1].humidity
+              dayTwoEL.children[4].textContent = "Wind: " + dataTwo.daily[1].wind_speed
+            
+              dayTwoEL.children[5].textContent = "Humidity: " + dataTwo.daily[1].humidity
+
+              let dayTwoIconCode = dataTwo.daily[2].weather[0].icon
+
+              dayTwoIconEL.src = `https://openweathermap.org/img/wn/${dayTwoIconCode}@2x.png`
+
+              dayTwoDesEl.textContent = dataTwo.daily[2].weather[0].description.toUpperCase()
             }
 
             function dayThree () {
 
-              dayThreeEL.children[0].textContent = data.list[19].dt_txt
+              dayThreeEL.children[2].textContent = data.list[19].dt_txt.substring(0, 10)
             
-              dayThreeEL.children[1].textContent = "Temp: " + dataTwo.daily[2].temp.day  
             
-              dayThreeEL.children[2].textContent = "Wind: " + dataTwo.daily[2].wind_speed
+              dayThreeEL.children[3].textContent = "Temp: " + dataTwo.daily[2].temp.day  
             
-              dayThreeEL.children[3].textContent = "Humidity: " + dataTwo.daily[2].humidity
+              dayThreeEL.children[4].textContent = "Wind: " + dataTwo.daily[2].wind_speed
+            
+              dayThreeEL.children[5].textContent = "Humidity: " + dataTwo.daily[2].humidity
+          
+
+            let dayThreeIconCode = dataTwo.daily[3].weather[0].icon
+
+            dayThreeIconEL.src = `https://openweathermap.org/img/wn/${dayThreeIconCode}@2x.png`
+
+            dayThreeDesEl.textContent = dataTwo.daily[3].weather[0].description.toUpperCase()
             }
 
             function dayFour () {
 
-              dayFourEL.children[0].textContent = data.list[27].dt_txt
+              dayFourEL.children[2].textContent = data.list[27].dt_txt.substring(0, 10)
             
-              dayFourEL.children[1].textContent = "Temp: " + dataTwo.daily[3].temp.day  
             
-              dayFourEL.children[2].textContent = "Wind: " + dataTwo.daily[3].wind_speed
+              dayFourEL.children[3].textContent = "Temp: " + dataTwo.daily[3].temp.day  
             
-              dayFourEL.children[3].textContent = "Humidity: " + dataTwo.daily[3].humidity
+              dayFourEL.children[4].textContent = "Wind: " + dataTwo.daily[3].wind_speed
+            
+              dayFourEL.children[5].textContent = "Humidity: " + dataTwo.daily[3].humidity
+
+              let dayFourIconCode = dataTwo.daily[4].weather[0].icon
+
+              dayFourIconEL.src = `https://openweathermap.org/img/wn/${dayFourIconCode}@2x.png`
+
+              dayFourDesEl.textContent = dataTwo.daily[4].weather[0].description.toUpperCase()
             }
 
             function dayFive () {
 
-              dayFiveEL.children[0].textContent = data.list[35].dt_txt
+              dayFiveEL.children[2].textContent = data.list[35].dt_txt.substring(0, 10)
             
-              dayFiveEL.children[1].textContent = "Temp: " + dataTwo.daily[4].temp.day  
+              dayFiveEL.children[3].textContent = "Temp: " + dataTwo.daily[4].temp.day  
             
-              dayFiveEL.children[2].textContent = "Wind: " + dataTwo.daily[4].wind_speed
+              dayFiveEL.children[4].textContent = "Wind: " + dataTwo.daily[4].wind_speed
             
-              dayFiveEL.children[3].textContent = "Humidity: " + dataTwo.daily[4].humidity
+              dayFiveEL.children[5].textContent = "Humidity: " + dataTwo.daily[4].humidity
+
+              let dayFiveIconCode = dataTwo.daily[5].weather[0].icon
+
+              dayFiveIconEL.src = `https://openweathermap.org/img/wn/${dayFiveIconCode}@2x.png`
+
+              dayFiveDesEl.textContent = dataTwo.daily[5].weather[0].description.toUpperCase()
             }
 
             dayOne();
@@ -124,6 +224,9 @@ searchBtn.addEventListener("click", function () {
             dayThree();
             dayFour();
             dayFive();
+
+            console.log(dataTwo)
+            console.log(data)
 
 
 
@@ -135,10 +238,16 @@ searchBtn.addEventListener("click", function () {
 
 // save search to local storage 
 
-let getCity = localStorage.getItem("city")
-        let getCityEL = document.createElement("h2")
+let storedCity = JSON.parse(localStorage.getItem("storedCity"))
 
-        getCityEL.textContent = getCity;
 
-        citySearch.appendChild(getCityEL);
-        
+if (storedCity !== null) 
+
+  for (let i = 0; i < storedCity.length; i++) {
+  
+    let listEl = document.createElement("li");
+    listEl.textContent = storedCity[i];
+    cityAppend.appendChild(listEl)
+    
+  }
+    
